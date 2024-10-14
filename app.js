6758527718b75640677af809bd8ca0734418ad86
dangalcan <megamagolas@gmail.com>
2024-10-14 23:44:02 +0200

    prepared main endpoint with backend and tool stuff

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-//TODO
+const Tools = require("./src/tools/index.js");
+const BackendDev = require('./src/backendDev/index.js')
+
+module.exports = {
+    Tools,
+    BackendDev
+}
--- src/backendDev/index.js
+++ src/backendDev/index.js
@@ -1,5 +1,4 @@
 const { handleValidation } = require("./middlewares/TrencherValidationHandlingMiddleware");
-
 const { CRUDRoutes,configureMulter,createRoute,getAllRoute,getRoute,updateRoute,patchRoute,deleteRoute,customGETRoute,customPOSTRoute,customPUTRoute,customPATCHRoute,customDELETERoute, } = require("./routing/TrencherRoutesHelper")
 
 module.exports = {
